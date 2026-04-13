fa27825897a580b8bca0a29bfffd0aa0aa78fd67
padvictor <victormmp@msn.com>
2026-04-13 14:31:23 -0300

    Adicionando slider e botao

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ import streamlit as st
 
 st.header('Jogando uma moeda')
 
+# Cria o controle deslizante (mínimo 1, máximo 1000, padrão 10)
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+
+# Cria o botão
+start_button = st.button('Executar')
+
+# Se o botão for clicado, executa o código abaixo
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+
 st.write('Ainda não é um aplicativo funcional. Em construção.')
